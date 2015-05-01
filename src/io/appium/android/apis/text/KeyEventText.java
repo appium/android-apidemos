ab2c3e7d5e44df1e09937474006df8100cd1003f
Isaac Murchie <isaac@saucelabs.com>
2015-05-01 15:40:57 -0700

    Add view to display key events

---
+++ src/io/appium/android/apis/text/KeyEventText.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.appium.android.apis.text;
+
+import io.appium.android.apis.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.KeyEvent;
+
+/**
+ * Using a LogTextBox to display a scrollable text area
+ * to which text is appended.
+ *
+ */
+public class KeyEventText extends Activity {
+
+    private LogTextBox mText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.key_event_text);
+
+        mText = (LogTextBox) findViewById(R.id.text);
+
+        Button clearButton = (Button) findViewById(R.id.clear);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mText.setText("");
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        mText.append("[keycode=" + String.valueOf(keyCode) + "] " + event.toString());
+        mText.append("\n\n");
+        return true;
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        mText.append("[keycode=" + String.valueOf(keyCode) + "] " + event.toString());
+        mText.append("\n\n");
+        return true;
+    }
+}
