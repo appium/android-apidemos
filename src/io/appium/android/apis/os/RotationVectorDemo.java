0d7fd31befe3b4e5687ff6b9a4536343f5b5d1f0
tzutalin <tzu.ta.lin@gmail.com>
2016-06-28 12:18:28 +0800

    Fix the wrong identity matrix for initial 4x4 matrix

--- src/io/appium/android/apis/os/RotationVectorDemo.java
+++ src/io/appium/android/apis/os/RotationVectorDemo.java
@@ -89,11 +89,17 @@ public class RotationVectorDemo extends Activity {
                     Sensor.TYPE_ROTATION_VECTOR);
 
             mCube = new Cube();
-            // initialize the rotation matrix to identity
-            mRotationMatrix[ 0] = 1;
-            mRotationMatrix[ 4] = 1;
-            mRotationMatrix[ 8] = 1;
-            mRotationMatrix[12] = 1;
+            // initialize the rotation matrix to identity,and it's 4x4 row-major matrix
+            /*
+                /  M[ 0]   M[ 1]   M[ 2]   M[ 3]  \
+                |  M[ 4]   M[ 5]   M[ 6]   M[ 7]  |
+                |  M[ 8]   M[ 9]   M[10]   M[11]  |
+                \  M[12]   M[13]   M[14]   M[15]  /
+            */
+            mRotationMatrix[0] = 1;
+            mRotationMatrix[5] = 1;
+            mRotationMatrix[10] = 1;
+            mRotationMatrix[15] = 1;
         }
 
         public void start() {
