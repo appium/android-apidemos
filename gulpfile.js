459d0017365207889aa81a173bac65a56cb6d9d9
Mykola Mokhnach <mokhnach@gmail.com>
2022-01-21 10:59:54 +0100

    refactor!: Switch the package to use Appium2 modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const gulp = require('gulp');
-const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+const boilerplate = require('@appium/gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
   build: 'android-apidemos',
+  files: ['index.js', '!gulpfile.js'],
 });
