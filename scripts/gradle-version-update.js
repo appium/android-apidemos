f883929a05010690912165864da92685beba0474
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2026-01-27 21:53:15 -0800

    fix lint

--- scripts/gradle-version-update.js
+++ scripts/gradle-version-update.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 const semver = require('semver');
 
 const VERSION_NAME_PATTERN = /^\s*versionName\s+['"](.+)['"]$/gm;
